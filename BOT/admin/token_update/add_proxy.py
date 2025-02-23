1bb101076422e980293b82224cb142667c2a98e8
Xssgit47 <xarteastmin@gmail.com>
2025-02-23 22:00:16 +0530

    Update add_proxy.py

--- BOT/admin/token_update/add_proxy.py
+++ BOT/admin/token_update/add_proxy.py
@@ -6,7 +6,7 @@ from FUNC.usersdb_func import *
 async def addbrod(Client, message):
     try:
         user_id     = str(message.from_user.id)
-        OWNER_ID    = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["7345260405"]
+        OWNER_ID    = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["OWNER_ID"]
         if user_id not in OWNER_ID:
             resp = """<b>You Don't Have Permission To Use This Command.    
 Contact Bot Owner @FNxDANGER</b>"""
