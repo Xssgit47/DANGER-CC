f1c740beb6bd2db827d278e463b153bde5062fbb
Xssgit47 <xarteastmin@gmail.com>
2025-02-23 22:23:32 +0530

    Update SG.py

--- SG.py
+++ SG.py
@@ -6,7 +6,7 @@ with open("FILES/config.json", "r",encoding="utf-8") as f:
     API_ID       = DATA["23418117"]
     API_HASH     = DATA["e93479e4b3eae090bf90a978b14ffc09"]
     BOT_TOKEN    = DATA["7595971863:AAEpBqA2OGRMxxXLpNsQmhAxuVEnW464AhQ"]
-    PHONE_NUMBER = DATA["9737505075"]
+    PHONE_NUMBER = DATA["9685745075"]
 
 user = Client("Scrapper",
               api_id       = API_ID,
