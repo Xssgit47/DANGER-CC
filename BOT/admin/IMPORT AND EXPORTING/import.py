0e51edff0942fe0dcc0907c0738794193f409266
Xssgit47 <xarteastmin@gmail.com>
2025-02-23 21:56:12 +0530

    Update import.py

--- BOT/admin/IMPORT AND EXPORTING/import.py
+++ BOT/admin/IMPORT AND EXPORTING/import.py
@@ -17,7 +17,7 @@ gc_db = folder.GCDB
 async def stats(Client, message):
     try:
         user_id = str(message.from_user.id)
-        OWNER_ID = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["7345260405"]
+        OWNER_ID = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["OWNER_ID"]
         if user_id not in OWNER_ID:
             resp = """<b>You Don't Have Permission To Use This Command.    
 Contact Bot Owner @FNxDANGER</b>"""
